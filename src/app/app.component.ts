5be6921575aa8d763e18ee9dd276f6a64b21efde
Justin Hornsby <justin.hornsby@entelect.co.za>
2022-12-05 13:12:44 +0200

    Done with assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Assignment-3';
+  visible: boolean = false;
+  clickLogs: number[] = [];
+
+  toggleVisibility() {
+    if(this.visible === false) {
+      this.visible = true;
+    }
+
+    else {
+      this.visible = false;
+    }
+
+    this.clickLogs.push(this.clickLogs.length + 1);
+  }
+
+  addClickEvent() {
+    const currentTime = Date.now();
+    this.clickLogs.push(currentTime);
+  }
 }
